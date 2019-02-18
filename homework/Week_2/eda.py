cbbbfe5e37346f604c421cda7e99347579fd22f9
unknown <daanvanlanschot@gmail.com>
2019-02-18 12:50:26 +0100

    Dag2

--- homework/Week_2/eda.py
+++ homework/Week_2/eda.py
@@ -15,7 +15,7 @@ import pandas as pd
 INPUT_CSV = "input.csv"
 
 # Load data into dataframe
-data_pd = pd.read_csv(INPUT_CSV)
+data_pd = pd.read_csv(INPUT_CSV, decimal=',')
 data_pd_columns = data_pd[["Country", "Region", "Pop. Density (per sq. mi.)", "Infant mortality (per 1000 births)", "GDP ($ per capita) dollars"]]
 
 # Empty "unknown" cells, then remove incomplete data
@@ -25,8 +25,17 @@ data_pd_clean = data_pd_clean.dropna()
 # Remove excess whitespace from region column
 data_pd_clean["Region"] = data_pd_clean["Region"].str.strip()
 
-# Convert values to floats and emove countries with outliers
+# Remove word dollar from GDP list
+data_pd_clean["GDP ($ per capita) dollars"] = data_pd_clean["GDP ($ per capita) dollars"].str.rstrip(' dollars')
 
+# Convert dtype to numeric
+data_pd_clean["Pop. Density (per sq. mi.)"] = data_pd_clean["Pop. Density (per sq. mi.)"].str.replace(',', '.')
+data_pd_clean = data_pd_clean.apply(pd.to_numeric, errors ='ignore')
 
+print(data_pd_clean.dtypes)
 
-print(data_pd_clean)
+# Plot the data
+ax = plt.gca()
+data_pd_clean.plot(kind = 'scatter', x = 'Infant mortality (per 1000 births)', y = 'GDP ($ per capita) dollars', ax = ax)
+#data_pd_clean.plot(kind ='line', x = 'Country', y = 'Infant mortality (per 1000 births)', color = 'red', ax = ax)
+plt.show()
